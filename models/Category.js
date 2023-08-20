7d3780769f81ccd7f8bee2f0e5cdf058a3e9317d
Tommy Ho <tommyho@Tommys-MBP.attlocal.net>
2023-08-20 18:30:48 -0500

    edited category and index

---
+++ models/Category.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+// Define the association
+Category.associate = (models) => {
+  Category.hasMany(models.Product, {
+    foreignKey: 'category_id'
+  });
+};
+
+module.exports = Category;
