46ce936a3bade003cf5f2a8cf9d69c7c437b852f
Tommy Ho <tommyho@Tommys-MBP.attlocal.net>
2023-08-20 19:26:27 -0500

    added all necessary files

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,10 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
+const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
--- models/product-route.js
+++ models/product-route.js
@@ -9,38 +9,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    product_name: {
-      type: DataTypes.STRING,
-      allownull: false
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category', // The name of the referenced model
-        key: 'id'
-      }
-    }
+    // define columns
   },
   {
     sequelize,
@@ -50,16 +19,5 @@ Product.init(
     modelName: 'product',
   }
 );
-
-Product.associate = (models) => {
-  Product.belongsTo(models.Category, {
-    foreignKey: 'category_id'
-  });
-
-  Product.belongsToMany(models.Tag, {
-    through: 'ProductTag',
-    foreignKey: 'product_id'
-  });
-};
 
 module.exports = Product;
