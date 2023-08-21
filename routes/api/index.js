a4af9b1277c9debc0ec9cc7487b6307a892b53af
Tommy Ho <tommyho@Tommys-MBP.attlocal.net>
2023-08-20 21:55:26 -0500

    corrected all errors

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
-
-module.exports = router;
+router.use('/categories', categoryRoutes);
